99b11e1cb6456a2f7bf006aa7d746185208746c4
uwesterr <uwe.sterr@st2c.degit config --global user.name napon123>
2020-04-11 12:43:42 +0200

    can produce plot predicted vs RKI data for all bundesländer and different optimizers

--- ValidationScriptBundeslaender.R
+++ ValidationScriptBundeslaender.R
@@ -1,10 +1,14 @@
 # function to calculate R0 and n0_erfasst
 library("tictoc")
 library(microbenchmark)
+library(staTools)
 source(file = "src/createDfBundLandKreis.R")
 source(file = "src/optimizerLoopingR0N0.R")
 source(file = "src/RechenkernSpeedUp.R")
 source(file = "src/Rechenkern.R")
+source(file = "src/optimizerGeneticAlgorithm.R")
+
+
 
 load("data/inputExample.RData")
 
@@ -14,6 +18,7 @@ historyDfBundesLand <- outpput[[2]]
 historyDfLandkreis <- outpput[[3]]
 input <- isolate(reactiveValuesToList(inputExample))
 optimizeFunction <- optimizerLoopingR0N0
+
 
 optWrap <- function(df, input) {
   df <- df %>% rename_at(vars(contains("sumAnzahlFall")), ~ "SumAnzahl" ) %>% 
@@ -80,100 +85,128 @@ optWrap <- function(df, input) {
 
 
 calcBerechnetValues <- function(R0, n0, input, startDate){
-
-dfRoNoOpt$R0<- R0
-dfRoNoOpt$n0_erfasst <-n0
-dfRechenKern <- (Rechenkern(dfRoNoOpt, input, startDate))
-
-return(dfRechenKern) 
+  
+  dfRoNoOpt$R0<- R0
+  dfRoNoOpt$n0_erfasst <-n0
+  dfRechenKern <- (Rechenkern(dfRoNoOpt, input, startDate))
+  
+  return(dfRechenKern) 
 }
 
 
 calcMetric <- function(dfRechenKern, data){
   res <- left_join(data, dfRechenKern, by = c("MeldeDate" = "Tag")) %>% filter(!is.na(ErfassteInfizierteBerechnet))
   (sum(log10(res$sumAnzahlFallBundesland) 
-        - log10(res$ErfassteInfizierteBerechnet) )^2)/(nrow(data)-1)^0.5
-
+       - log10(res$ErfassteInfizierteBerechnet) )^2)/(nrow(data)-1)^0.5
+  
 }
 
-#createLandkreisR0_no_erfasstDf <- function(df, historyDfBund, regionSelected, vals, input,session,optimizeFunction = optimizerLoopingR0N0, ...  ){
-  df <- historyDfBundesLand
-  dfTotal <- df %>% rename("whichRegion" = "Bundesland") # %>% as_tibble %>%  mutate(a = optimizerLoopingR0N0 )
-  dfTotalNested <-  dfTotal %>% group_by(whichRegion) %>% nest
+df <- historyDfBundesLand
+dfTotal <- df %>% rename("whichRegion" = "Bundesland") # %>% as_tibble %>%  mutate(a = optimizerLoopingR0N0 )
+dfTotalNested <-  dfTotal %>% group_by(whichRegion) %>% nest
+
+#######################  set optimizer function  ##########################
+  source(file = "src/optimizerLoopingR0N0.R")
+  optimizeFunction <- optimizerLoopingR0N0
  tic()
-#R0N0Optvalues <-  dfTotalNested %>% mutate(optimizedValues = map(data, optWrap, input))
+R0N0Optvalues <-  dfTotalNested %>% mutate(optimizedValues = map(data, optWrap, input))
  
-#save(R0N0Optvalues, file = "R0N0Optvalues.RData")
- load("a.RData")
- toc()
- MetricDf <- R0N0Optvalues %>% mutate(n0Opt = (optimizedValues[[1]][["n0Opt"]]) %>% as.numeric(),
-                   R0Opt = (optimizedValues[[1]][["R0Opt"]]) %>% as.numeric(),
-                   dfRechenKern = map2(R0Opt, n0Opt, calcBerechnetValues, input, startDate),
-                   metric = map2_dbl(dfRechenKern, data, calcMetric))
+save(R0N0Optvalues, file = "R0N0Optvalues.RData")
+# #load("R0N0Optvalues.RData")
+# toc()
+ MetricDfLoopingR0N0 <- R0N0Optvalues %>% mutate(n0Opt = (optimizedValues[[1]][["n0Opt"]]) %>% as.numeric(),
+                                                 R0Opt = (optimizedValues[[1]][["R0Opt"]]) %>% as.numeric(),
+                                                 dfRechenKern = map2(R0Opt, n0Opt, calcBerechnetValues, input, startDate),
+                                                 metric = map2_dbl(dfRechenKern, data, calcMetric),
+                                                 SumAnzahlFall = data[[1]][["AnzahlFall"]] %>% sum,
+                                                 OptimizerFunction = "LoopingR0N0")
  
- MetricDf %>% ungroup() %>% mutate(whichRegion= fct_reorder(whichRegion,metric))  %>%  
-   ggplot(aes(whichRegion, metric, color = whichRegion)) + geom_point() + theme(legend.position = "none") +
-   coord_flip() + labs(title = expression("Metric"), y = "Metric", x = "")
-                       
-  
  
-################## optimizer #################
- library(GA)
- 
- calcPredictionsForOptimization = function(reduzierung_rt1, reduzierung_rt2, reduzierung_rt3, R0Opt, n0Opt, startDate) 
- {
-   input$reduzierung_rt1 = reduzierung_rt1
-   input$reduzierung_rt2 = reduzierung_rt2
-   input$reduzierung_rt3 = reduzierung_rt3
-  # browser()
-   res <- calcBerechnetValues(R0Opt, n0Opt, input, startDate)
-   metric <- calcMetric(res, MetricDf$data[[2]])
-   cat("metric: ", metric)
-return(-metric) # minus because GA maximizes
- } 
- 
- tic()
- GA <- ga(type = "real-valued", 
-          fitness =  function(x) calcPredictionsForOptimization(x[1], x[2], x[3], MetricDf$R0Opt[[1]],  MetricDf$n0Opt[[1]], startDate),
-        
-          lower = c(20, 30,-20 ), upper = c(40, 50, 20), 
-          popSize = 10, maxiter = 30, run = 5)
- toc()
- # n0_erfasst_start = n0_erfasst_start)
- summary(GA)
- plot(GA)
-# 
-# 
-# 
-# 
-# 
-# 
-# 
-#   
-# dfRoNoOpt$R0<- 1.3
-# 
-# dfRoNoOpt$n0_erfasst <-200
+ save(MetricDfLoopingR0N0, file = "data/MetricDfLoopingR0N0.RData")
+load("data/MetricDfLoopingR0N0.RData")
+MetricDfLoopingR0N0 %>% ungroup() %>% mutate(whichRegion= fct_reorder(whichRegion,metricLoopingR0N0))  %>%  
+  ggplot(aes(whichRegion, metricLoopingR0N0, color = whichRegion)) + geom_point() + theme(legend.position = "none") +
+  coord_flip() + labs(title = expression("Metric"), y = "Metric", x = "")
+
+#######################  set optimizer function  ##########################
+source(file = "src/optimizerGeneticAlgorithm.R")
+optimizeFunction <- optimizerGeneticAlgorithm
+tic()
+optimizerGeneticAlgorithmRmsDf <-  dfTotalNested %>% mutate(optimizedValues = map(data, optWrap, input))
+save(optimizerGeneticAlgorithmRmsDf, file = "optimizerGeneticAlgorithmRmsDf.RData")
+#load("R0N0Optvalues.RData")
+toc()  
+MetricGeneticAlgorithmRmsDf <- optimizerGeneticAlgorithmRmsDf %>% mutate(n0Opt = (optimizedValues[[1]][["n0Opt"]]) %>% as.numeric(),
+                                                                 R0Opt = (optimizedValues[[1]][["R0Opt"]]) %>% as.numeric(),
+                                                                 dfRechenKern = map2(R0Opt, n0Opt, calcBerechnetValues, input, startDate),
+                                                                 metric = map2_dbl(dfRechenKern, data, calcMetric),
+                                                                 SumAnzahlFall = data[[1]][["AnzahlFall"]] %>% sum,
+                                                                 OptimizerFunction = "optimizerGeneticAlgorithm")
+
+save(MetricGeneticAlgorithmRmsDf, file = "data/MetricGeneticAlgorithmRmsDf.RData")
+load("data/MetricGeneticAlgorithmRmsDf.RData")
+
+MetricGeneticAlgorithmRmsDf %>% ungroup() %>% mutate(whichRegion= fct_reorder(whichRegion,metricGeneticAlgorithm))  %>%  
+  ggplot(aes(whichRegion, metricGeneticAlgorithm, color = whichRegion)) + geom_point() + theme(legend.position = "none") +
+  coord_flip() + labs(title = expression("optimizerGeneticAlgorithm Metric"), y = "Metric", x = "")
+
+
+
+
+################### compare the two optimizers
+
+
+compareOptimizerDf <- rbind(MetricDfLoopingR0N0, MetricGeneticAlgorithmRmsDf)
+compareOptimizerDfLong <- compareOptimizerDf %>% pivot_longer(OptimizerFunction, names_to = "OptimizingFunction", values_to =  "Function")
+compareOptimizerDfLong %>% ggplot(aes(whichRegion,R0Opt, color = Function)) +geom_point() + coord_flip() + 
+  labs(title = ("Comparision optimized R0"))
+
+compareOptimizerDfLong %>% ggplot(aes(whichRegion, metric, color = Function)) +geom_point() + coord_flip() + 
+  labs(title = ("Comparision metric"))
+
+
+
+#################### check MPE vs rms with GA  ######################
+
+#######################  set optimizer function  ##########################
+source(file = "src/optimizerGeneticAlgorithm.R")
+optimizeFunction <- optimizerGeneticAlgorithm
+tic()
+optimizerGeneticAlgorithmMPE <-  dfTotalNested %>% mutate(optimizedValues = map(data, optWrap, input))
+save(optimizerGeneticAlgorithmMPE, file = "optimizerGeneticAlgorithmMPE.RData")
+#load("R0N0Optvalues.RData")
+toc()  
+MetricGeneticAlgorithmMpeDf <- optimizerGeneticAlgorithmMPE %>% mutate(n0Opt = (optimizedValues[[1]][["n0Opt"]]) %>% as.numeric(),
+                                                                 R0Opt = (optimizedValues[[1]][["R0Opt"]]) %>% as.numeric(),
+                                                                 dfRechenKern = map2(R0Opt, n0Opt, calcBerechnetValues, input, startDate),
+                                                                 metric = map2_dbl(dfRechenKern, data, calcMetric),
+                                                                 SumAnzahlFall = data[[1]][["AnzahlFall"]] %>% sum,
+                                                                 OptimizerFunction = "optimizerGeneticAlgorithmMPE")
+
+save(MetricGeneticAlgorithmMpeDf, file = "data/MetricGeneticAlgorithmMpeDf.RData")
+
+################### compare MPE vs rms optimizers
+
+#compareOptimizerDf <- left_join(MetricGeneticAlgorithmMpeDf, MetricGeneticAlgorithmRmsDf %>% select(-data), by = "whichRegion", suffix = c("_GA_MPE", "_GA_rms"))
+#compareOptimizerDf <- left_join(compareOptimizerDf, MetricDfLoopingR0N0 %>% select(-data), by = "whichRegion", suffix = c("_GA", "_Loop"))
 #
-# source(file = "src/RechenkernSpeedUp.R")
-#
-# dfRechenKernSpeed <- isolate(Rechenkern(dfRoNoOpt, input, startDate)) 
-# source(file = "src/Rechenkern.R")
-# dfRechenKern <- isolate(Rechenkern(dfRoNoOpt, input, startDate))
-#
-#
-# all.equal( dfRechenKernSpeed, dfRechenKern)
-# source(file = "src/RechenkernSpeedUp.R")
-# microbenchmark( 
-#   
-# 
-#   dfRechenKernSpeed <- isolate(Rechenkern(dfRoNoOpt, input, startDate))
-#   , times = 10)
-# 
-# source(file = "src/Rechenkern.R")
-# microbenchmark( 
-#   
-#
-#   dfRechenKern <- isolate(Rechenkern(dfRoNoOpt, input, startDate))
-#   , times = 10)
-#            
-# #
+compareOptimizerDf <- rbind(MetricGeneticAlgorithmMpeDf, MetricGeneticAlgorithmRmsDf, MetricDfLoopingR0N0)
+
+
+compareOptimizerDf %>% ungroup() %>% mutate(whichRegion= fct_reorder(whichRegion,R0Opt))  %>% 
+  ggplot(aes(whichRegion,R0Opt, color = OptimizerFunction)) +geom_point() + coord_flip() + 
+  labs(title = ("Comparision optimized R0"))
+
+compareOptimizerDf %>%  ungroup() %>% mutate(whichRegion= fct_reorder(whichRegion, metric))  %>% 
+  ggplot(aes(whichRegion, metric, color = OptimizerFunction)) +geom_point() + coord_flip() + 
+  labs(title = ("Comparision metric"))
+
+
+
+compareOptimizerUnnestDf <-  compareOptimizerDf %>% unnest(data) %>%  unnest(dfRechenKern, sep = "_")
+
+
+compareOptimizerUnnestDf  %>% ggplot(aes(MeldeDate, sumAnzahlFallBundesland, color = OptimizerFunction)) + geom_point() + 
+ geom_point(aes(Tag, ErfassteInfizierteBerechnet))  +
+  facet_wrap(vars(whichRegion)) 
+
