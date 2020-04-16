c6122f564cb7be811c5a2313d2a427d1d5347c41
uwesterr <uwe.sterr@st2c.degit config --global user.name napon123>
2020-04-16 11:26:41 +0200

    added normalization of optimzation parameters

--- src/helperForCovid19.R
+++ src/helperForCovid19.R
@@ -43,20 +43,78 @@ calcMetric <- function(dfRechenKern, data){
 
 ################    optimizing reduzierung  #################
 
+createOptParmeters <- function(){
+  
+  parameter_tibble <- tribble(
+    ~var_name,         ~var_value, ~var_min,  ~var_max,  ~var_selected,
+    "reduzierung_rt1", 0         ,  0,        60,        "TRUE",
+    "reduzierung_rt2", 0         ,  0,        60,        "TRUE",
+    "reduzierung_rt3", -20       ,  -40,      30,        "TRUE")
+  
+  parameter_tibble$var_selected <- TRUE
+  # parameter_tibble[parameter_tibble$var_name =="TNOM", "var_selected"] <- "FALSE"
+  
+  parameter_tibble$new_sol <- NA 
+  
+  parameter_tibble <- parameter_tibble %>% mutate(start_value = var_value)
+  
+  parameter_tibble <- parameter_tibble %>% mutate(var_value_norm = (var_value - var_min) /(var_max-var_min))
+  
+  optPara <- parameter_tibble %>% filter(var_selected == TRUE) %>% 
+    select(var_value) %>% unlist
+  names(optPara) <- parameter_tibble %>% filter(var_selected == TRUE) %>% 
+    select(var_name) %>% unlist
+  nonOptPara <- parameter_tibble %>% filter(var_selected == FALSE) %>% 
+    select(var_value) %>% unlist
+  names(nonOptPara) <-parameter_tibble %>% filter(var_selected == FALSE) %>% 
+    select(var_name) %>% unlist
+  allPara <- c(optPara , nonOptPara )
+  maxOpt <- parameter_tibble %>% filter(var_selected == TRUE) %>% 
+    select(var_max) %>% unlist
+  minOpt <- parameter_tibble %>% filter(var_selected == TRUE) %>% 
+    select(var_min) %>% unlist
+  parameter_tibble <-  parameter_tibble %>% filter(var_selected == TRUE) %>% 
+    mutate( suggestions_norm =  (var_value - var_min) /(var_max-var_min))
+  
+  suggestions <- parameter_tibble  %>% filter(var_selected == TRUE)%>%
+    select(suggestions_norm) %>%unlist
+  
+  minOpt <- rep(0, length(minOpt))
+  maxOpt <- rep(1, length(minOpt))
+  # browser()
+  return(list(allPara, optPara, minOpt, maxOpt, suggestions, parameter_tibble  ))
+}
+
+denormalizePara <- function(optPara, parameter_tibble, para) {
+  denormPara <- list(NULL)
+  # browser()
+  for(i in 1 : length(optPara)){
+      denormPara[[i]] <- optPara[[i]] * (parameter_tibble$var_max[i]-parameter_tibble$var_min[i]) +
+        parameter_tibble$var_min[i]
+  }
+  
+  # browser()
+  names(denormPara) <- parameter_tibble$var_name
+  denormPara
+}
 optimizerGeneticAlgorithmRedReduction <- function(RkiDataWithR0N0, input) {
   # optimizer using genetic algorithm to optimize reduzierungsmaßnahmen und R0 n0
   # dfRoNoOpt should be dataframe starting with reduzierung_datum1
   dateOfFirstAction <- input$reduzierung_datum1
    RkiDataWithR0N0 <- RkiDataWithR0N0 %>% filter(MeldeDate >= dateOfFirstAction) # only consider values after first reduction action
   
-  calcPredictionsForGaOptimization = function(reduzierung_rt1, reduzierung_rt2, reduzierung_rt3, RkiDataWithR0N0, input) {
-    
-    
-
+ # calcPredictionsForGaOptimization = function(reduzierung_rt1, reduzierung_rt2, reduzierung_rt3, RkiDataWithR0N0, input) {
+   calcPredictionsForGaOptimization = function(optPara, allPara, parameter_tibble, input) {   
+   # browser()
+     denormPara <- denormalizePara(optPara, parameter_tibble)
+   #  cat("para",unlist(denormPara,"\n"))
+     for(i in 1 : length(optPara)){
+       allPara[[i]] <- denormPara[[i]]
+     }
     inputForOptimization <- input# to make setting reduzierung_rtx easy and fast
-    inputForOptimization$reduzierung_rt1 <- reduzierung_rt1
-    inputForOptimization$reduzierung_rt2 <- reduzierung_rt2
-    inputForOptimization$reduzierung_rt3 <- reduzierung_rt3
+    inputForOptimization$reduzierung_rt1 <- allPara[["reduzierung_rt1"]]
+    inputForOptimization$reduzierung_rt2 <- allPara[["reduzierung_rt2"]]
+    inputForOptimization$reduzierung_rt3 <- allPara[["reduzierung_rt3"]]
     
     inputForOptimization$dateInput[2] = RkiDataWithR0N0$MeldeDate %>% max() # set endDate to date of last MeldeDate
     
@@ -64,26 +122,60 @@ optimizerGeneticAlgorithmRedReduction <- function(RkiDataWithR0N0, input) {
     dfRechenKern <-   Rechenkern(RkiDataWithR0N0, inputForOptimization)
     dfRechenKern <- dfRechenKern %>% filter(Tag  %in% RkiDataWithR0N0$MeldeDate)
     RkiDataWithR0N0 <- RkiDataWithR0N0 %>% filter(MeldeDate  %in% dfRechenKern$Tag)
-    res <- MPE(dfRechenKern$ErfassteInfizierteBerechnet,RkiDataWithR0N0$SumAnzahl)
+    # res <- MPE(dfRechenKern$ErfassteInfizierteBerechnet,RkiDataWithR0N0$SumAnzahl)
     res <- (sum(log10(RkiDataWithR0N0$SumAnzahl)   - log10(dfRechenKern$ErfassteInfizierteBerechnet) )^2)/(nrow(dfRechenKern)-1)^0.5
 
     # cat("res is :", res , "redu1 = ", reduzierung_rt1, "\n")
     # res <- sqrt(mean((log10(dfRechenKern$ErfassteInfizierteBerechnet)-log10(RkiDataWithR0N0$SumAnzahl))^2))
     return(-res)
   } 
+   res <- createOptParmeters()
+   allPara          <- res[[1]]
+   optPara          <- res[[2]]
+   minOpt           <- res[[3]]
+   maxOpt           <- res[[4]]
+   suggestions      <- res[[5]]
+   parameter_tibble <- res[[6]]
   
-  suggestions <- c( 0, 0, -20)
-  GA <- ga(type = "real-valued", 
-           fitness =  function(x) calcPredictionsForGaOptimization(x[1], x[2], x[3], RkiDataWithR0N0, input),
-           suggestions =suggestions,
-           lower = c(0, 0, -40), upper = c(60, 60, 30), 
-           popSize = 10, maxiter = 30, run = 5, seed = 2020)
+  GA <-  ga(type = "real-valued",
+            suggestions = suggestions,
+            fitness = calcPredictionsForGaOptimization,
+            lower = minOpt,
+            upper = maxOpt,
+            popSize = 10, maxiter = 10,
+            seed = 2020,
+            allPara = allPara, parameter_tibble = parameter_tibble, input = input
+  )
+  denormPara <- denormalizePara(GA@solution, parameter_tibble, para)
   
-  input$reduzierung_rt1 <- GA@solution[[1]]
-  input$reduzierung_rt2 <- GA@solution[[2]]
-  input$reduzierung_rt3 <- GA@solution[[3]]
-  reduzierungsOptResult <- tibble("reduzierung_rt1" = GA@solution[[1]], "reduzierung_rt2" = GA@solution[[2]],
-                                "reduzierung_rt3" = GA@solution[[3]], "GaPara" = list(GA))
+  print(GA@solution)
+  for(i in seq(1, length(denormPara)))  {
+    print(i)
+  GA@solution[i] <- denormPara[[i]]
+  }
+  print(GA@solution)
+  
+  browser()
+  
+  #cat("denormPara", unlist(denormPara), "\n")
+  
+#  ############## non normalized optimization
+#  suggestions <- c( 0, 0, -20)
+#  GA <- ga(type = "real-valued", 
+#           fitness =  function(x) calcPredictionsForGaOptimization(x[1], x[2], x[3], RkiDataWithR0N0, input),
+#           suggestions =suggestions,
+#           lower = c(0, 0, -40), upper = c(60, 60, 30), 
+#           popSize = 10, maxiter = 30, run = 5, seed = 2020,
+#           keepBest = TRUE) #a logical argument specifying if best solutions at each iteration should be saved in a slot called bestSol
+#  
+#  input$reduzierung_rt1 <- GA@solution[[1]]
+#  input$reduzierung_rt2 <- GA@solution[[2]]
+#  input$reduzierung_rt3 <- GA@solution[[3]]
+#  print(GA@solution)
+#  browser()
+  #########################################
+  reduzierungsOptResult <- tibble("reduzierung_rt1" = denormPara[["reduzierung_rt1"]], "reduzierung_rt2" = denormPara[["reduzierung_rt2"]],
+                                "reduzierung_rt3" = denormPara[["reduzierung_rt3"]], "GaPara" = list(summary(GA)))
   return(list("reduzierungsOptResult" = reduzierungsOptResult))
   
 }
